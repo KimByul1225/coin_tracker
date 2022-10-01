2125c48d013e9b5d836ddceaa63f52f404adb4ef
ByulKim <kimbyul1225@gmail.com>
2022-10-01 15:18:23 +0900

    coin list 페이지 컴포넌트 생성

---
+++ src/components/TableBody.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+function TableBody() {
+    return (
+        <div>
+            
+        </div>
+    );
+}
+
+export default TableBody;
+
+
--- src/routes/CoinsList.tsx
+++ src/routes/CoinsList.tsx
@@ -8,6 +8,39 @@ export default function CoinsList() {
         <Container>
             <BrowserTitle title="Coin Tracker"/>
             <Title>Coin Tracker</Title>
+            <Wrap>
+                <Table>
+                    <TableHeader>
+                        <th>
+                            Rank
+                        </th>
+                        <th></th>
+                        <th>
+                            Volume / Change
+                        </th>
+                        <th>
+                            Price / Change
+                        </th>
+                    </TableHeader>
+
+                </Table>
+
+                {/* {allData?.map((coin, index) => (
+                <Coin
+                    key={`${coin.id}${index}`}
+                    id={coin.id}
+                    rank={coin.rank}
+                    symbol={coin.symbol}
+                    name={coin.name}
+                    price={coin.quotes.USD.price}
+                    priceChange={coin.quotes.USD.percent_change_24h}
+                    volume={coin.quotes.USD.volume_24h}
+                    volumeChange={coin.quotes.USD.volume_24h_change_24h}
+                    image={`https://cryptocurrencyliveprices.com/img/${coin.id}.png`}
+                />
+                ))} */}
+
+            </Wrap>
         </Container>
     )
 }
@@ -30,3 +63,31 @@ const Title = styled.h1`
     font-size: 30px;
     margin-bottom: 30px;
 `;
+
+const Wrap = styled.div`
+    padding: 20px;
+`
+const Table = styled.table`
+    width: 100%;
+`
+const TableHeader = styled.thead`
+    color: ${(props) => props.theme.grayColor};
+    text-transform: uppercase;
+    font-size: 14px;
+    font-weight: 600;
+    width: 100%;
+    th:nth-child(1){
+        width: 20%;
+    }
+    th:nth-child(2){
+        width: 20%;
+    }
+    th:nth-child(3){
+        width: 30%;
+    }
+    th:nth-child(4){
+        width: 30%;
+    }
+`
+
+
