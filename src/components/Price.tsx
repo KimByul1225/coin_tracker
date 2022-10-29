0109feff564d61274ca4f2fb3b1ba0efab5ccb73
ByulKim <kimbyul1225@gmail.com>
2022-10-29 16:35:51 +0900

    스틱차트 1차 완료, data 없을 때 방어코드 필요

--- src/components/Price.tsx
+++ src/components/Price.tsx
@@ -4,6 +4,7 @@ import { fetchCoinHistory } from '../api';
 import ApexChart from "react-apexcharts";
 import { useRecoilValue } from 'recoil';
 import { isDarkAtom } from '../atoms';
+import Loading from './Loading';
 
 interface ChartProps{
     coinId: string;
@@ -23,18 +24,63 @@ interface IHistorycal {
 function Price({ coinId }: ChartProps) {
 
     const isDark = useRecoilValue(isDarkAtom)
-    const{ isLoading, data } = useQuery<IHistorycal[]>(["ohlcv", coinId], () => fetchCoinHistory(coinId),{
+    const{ isLoading, data } = useQuery<IHistorycal[]>(["ohlc", "price", coinId], () => fetchCoinHistory(coinId),{
         refetchInterval: 1000000,
     });
-    const mappingData = data?.map((price) => ({ x: price.time_open, y: [price.open, price.high, price.low, price.close] }));
+    const mappingData = data?.map((price) => ({ x: price.time_open, y: [Number(price.open), Number(price.high),Number(price.low), Number(price.close)] }));
 
-    console.log("priceData", data);
+
+    const sample = [
+        {
+            x: 1682035200,
+            y: [51.98, 56.29, 51.59, 53.85]
+        },
+        {
+            x: 1682121600,
+            y: [51.98, 56.29, 51.59, 53.85]
+        },
+        {
+            x: 1682208000,
+            y: [51.98, 56.29, 51.59, 53.85]
+        }
+    ]
+
+
     console.log("Mapping", mappingData);
 
+    if (isLoading) {
+        return <Loading />;
+    }
 
     return (
         <div>
-            Price
+            {
+                isLoading? "Loading..." 
+                : 
+                <>
+                    <ApexChart 
+                        type="candlestick"
+                        series={[{ data: mappingData ? mappingData : sample }]}
+                        height={400}
+                        options={{
+                            chart: {
+                                toolbar: { show: true, tools: { download: true, pan: false, reset: false, zoom: false, zoomin: false, zoomout: false } },
+                                background: isDark ? "black" : "white",
+                            },
+                            theme: { mode: isDark ? "dark" : "light" },
+                            title: { text: "CandleStick Chart", align: "center", style: { color: isDark ? "white" : "black" } },
+                            xaxis: { type: "datetime" },
+                            yaxis: {
+                                labels: { formatter: (value: number) => `$${value.toFixed(2)}` },
+                                axisBorder: { show: false },
+                                axisTicks: { show: false },
+                                tooltip: { enabled: true },
+                            },
+                        }}
+                        
+                    />
+                </>
+            }
         </div>
     );
 }
