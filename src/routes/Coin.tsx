f452b8449a79ab71e3ccf4e4b413d23f9a868b32
ByulKim <kimbyul1225@gmail.com>
2022-11-21 21:18:36 +0900

    탭메뉴 변경

--- src/routes/Coin.tsx
+++ src/routes/Coin.tsx
@@ -220,28 +220,30 @@ const NoData = styled.div`
 `
 
 const Tabs = styled.div`
-    display: grid;
-    grid-template-columns: repeat(2, 1fr);
-    margin: 25px 0px;
-    gap: 10px;
+    display: flex;
+    align-items: center;
+    justify-content: space-between;
+    margin: 10px 0px;
 `;
 
 const Tab = styled.span<{ isActive: boolean }>`
+    width: 48.5%;
     text-align: center;
     text-transform: uppercase;
-    font-size: 12px;
-    font-weight: 400;
+    font-size: 22px;
+    font-weight: 600;
     padding: 7px 0px;
     border-radius: 10px;
     
     a {
         display: block;
-        padding: 12px 10px;
+        padding: 10px 0;
         text-transform: uppercase;
-        background-color: ${(props) => (props.isActive === true ? props.theme.grayColor : props.theme.lightBlackColor)};
-        border-radius: 100px;
+        border: 1px solid #bababa;
+        border-radius: 10px;
+        background-color: ${(props) => (props.isActive ? props.theme.grayColor : "transparents")};
         &:hover {
-        background-color: ${(props) => props.theme.grayColor};
+        background-color: ${(props) => props.theme.lightBlackColor};
         }
     }
 
